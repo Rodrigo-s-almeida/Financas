5fa69bd589e7d57cbf5885d047571b247c18b2cb
Rodrigo-s-almeida <53126818+Rodrigo-s-almeida@users.noreply.github.com>
2021-10-23 15:38:45 -0400

    Update Calculadora_IMC.py

--- Calculadora_IMC.py
+++ Calculadora_IMC.py
@@ -26,11 +26,6 @@ elif imc < 25:
 else:
     st.write('Resultado: Você está acima do peso')    
 
-#im_magro = Image.open('C:/Python/Streamlit/magro.jpg')    
-#im_normal = Image.open('C:/Python/Streamlit/emoji_legal.jpg')    
-#im_gordo = Image.open('C:/Python/Streamlit/emoji_polegar_para_baixo.jpg')    
-
-
 im_magro = Image.open(requests.get('https://raw.githubusercontent.com/Rodrigo-s-almeida/Financas/main/magro.jpg', stream=True).raw)    
 im_normal = Image.open(requests.get('https://raw.githubusercontent.com/Rodrigo-s-almeida/Financas/main/emoji_legal.jpg', stream=True).raw)      
 im_gordo = Image.open(requests.get('https://raw.githubusercontent.com/Rodrigo-s-almeida/Financas/main/emoji_polegar_para_baixo.jpg', stream=True).raw) 
