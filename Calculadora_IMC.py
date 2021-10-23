61c3cc6ca79b4d266b7c34096f32d5f361389ab5
Rodrigo-s-almeida <53126818+Rodrigo-s-almeida@users.noreply.github.com>
2021-10-23 16:22:50 -0400

    Update Calculadora_IMC.py

--- Calculadora_IMC.py
+++ Calculadora_IMC.py
@@ -21,19 +21,34 @@ st.write('Seu IMC é de: ', round(imc, 1))
 
 if imc < 18.5:
     st.write('Resultado: Você está abaixo do peso ideal')
-elif imc < 25:
-    st.write('Resultado: Você está dentro do peso')
+elif imc < 24.9:
+    st.write('Resultado: Você está no peso ideal (Parabéns)')    
+elif imc < 29.9:
+    st.write('Resultado: Você está levemente acima do peso')
+elif imc < 34.9:
+    st.write('Resultado: Obesidade Grau I')
+elif imc < 39.9:
+    st.write('Resultado: Obesidade Grau II (SEVERA)')
 else:
-    st.write('Resultado: Você está acima do peso')    
+    st.write('Resultado: Obesidade Grau III (MÓRBIDA)')   
 
-im_magro = Image.open(requests.get('https://raw.githubusercontent.com/Rodrigo-s-almeida/Financas/main/magro.jpg', stream=True).raw)    
-im_normal = Image.open(requests.get('https://raw.githubusercontent.com/Rodrigo-s-almeida/Financas/main/emoji_legal.jpg', stream=True).raw)      
-im_gordo = Image.open(requests.get('https://raw.githubusercontent.com/Rodrigo-s-almeida/Financas/main/emoji_polegar_para_baixo.jpg', stream=True).raw) 
+im_magro = Image.open(requests.get('https://raw.githubusercontent.com/Rodrigo-s-almeida/Financas/main/emoji_magro.jpg', stream=True).raw)
+im_ideal = Image.open(requests.get('https://raw.githubusercontent.com/Rodrigo-s-almeida/Financas/main/emoji_ideal.jpg', stream=True).raw)   
+im_levemente = Image.open(requests.get('https://raw.githubusercontent.com/Rodrigo-s-almeida/Financas/main/emoji_levemente.jpg', stream=True).raw)
+im_grau1 = Image.open(requests.get('https://raw.githubusercontent.com/Rodrigo-s-almeida/Financas/main/emoji_grau1.jpg', stream=True).raw)
+im_grau2 = Image.open(requests.get('https://raw.githubusercontent.com/Rodrigo-s-almeida/Financas/main/emoji_grau2.jpg', stream=True).raw)
+im_grau3 = Image.open(requests.get('https://raw.githubusercontent.com/Rodrigo-s-almeida/Financas/main/emoji_grau3.jpg', stream=True).raw)
 
 
 if imc < 18.5:
     st.image(im_magro)
-elif imc < 25:
-    st.image(im_normal)
+elif imc < 24.9:
+    st.image(im_ideal)
+elif imc < 29.9:
+    st.image(im_levemente)
+elif imc < 34.9:
+    st.image(im_grau1)
+elif imc < 39.9:
+    st.image(im_grau2)   
 else:
-    st.image(im_gordo) 
+    st.image(im_grau3) 
